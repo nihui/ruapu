bd82c92df204c69e135b849a75b933c545fc3cc1
Yoh <wpz_yoh@163.com>
2024-02-28 11:25:54 +0000

    Enhance Python API to support retrieval of all available ISAs

--- ruapu-py.c
+++ ruapu-py.c
@@ -15,9 +15,35 @@ static PyObject *ruapu_supports_py(PyObject *self, PyObject *args, PyObject *kwa
     Py_RETURN_FALSE;
 }
 
+static PyObject *get_isa_items_py(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    int total = 0;
+    for (size_t i = 0; i < sizeof(g_ruapu_isa_map) / sizeof(g_ruapu_isa_map[0]); i++)
+    {
+        if (g_ruapu_isa_map[i].capable)
+        {
+            total ++;
+        }
+    }
+
+    PyObject* supported_isa = PyTuple_New(total);
+
+    int tuple_idx = 0;
+    for (size_t i = 0; i < sizeof(g_ruapu_isa_map) / sizeof(g_ruapu_isa_map[0]); i++)
+    {
+        if (g_ruapu_isa_map[i].capable)
+        {
+            PyTuple_SetItem(supported_isa, tuple_idx++, PyUnicode_FromString(g_ruapu_isa_map[i].isa));
+        }
+    }
+
+    return supported_isa;
+}
+
 static PyMethodDef ruapu_methods[] =
 {
     {"supports", ruapu_supports_py, METH_VARARGS | METH_KEYWORDS, "Check if the CPU supports an instruction set"},
+    {"rua", get_isa_items_py, METH_VARARGS | METH_KEYWORDS, "Get the instruction sets supported by the current CPU"},
     {NULL, NULL, 0, NULL}
 };
 
