f7108bae05c48014560e7bca2294e5cc392c0322
nihuini <nihuini@tencent.com>
2025-04-27 16:19:36 +0800

    detect arm sve2p1

--- ruapu.h
+++ ruapu.h
@@ -258,6 +258,7 @@ RUAPU_INSTCODE(mte, 0xd96003e0) // ldg x0,[sp]
 RUAPU_INSTCODE(mte2, 0xd9e003e0) // ldgm x0,[sp]
 RUAPU_INSTCODE(sve, 0x65608000) // fmad z0.h,p0/m,z0.h,z0.h
 RUAPU_INSTCODE(sve2, 0x44405000) // smlslb z0.h,z0.b,z0.b
+RUAPU_INSTCODE(sve2p1, 0x04052000) // addqv v0.16b,p0,z0.b
 RUAPU_INSTCODE(svebf16, 0x6460e400) // bfmmla z0.s,z0.h,z0.h
 RUAPU_INSTCODE(svei8mm, 0x45009800) // smmla z0.s,z0.b,z0.b
 RUAPU_INSTCODE(svef32mm, 0x64a0e400) // fmmla z0.s,z0.s,z0.s
@@ -486,6 +487,7 @@ RUAPU_ISAENTRY(mte)
 RUAPU_ISAENTRY(mte2)
 RUAPU_ISAENTRY(sve)
 RUAPU_ISAENTRY(sve2)
+RUAPU_ISAENTRY(sve2p1)
 RUAPU_ISAENTRY(svebf16)
 RUAPU_ISAENTRY(svei8mm)
 RUAPU_ISAENTRY(svef32mm)
